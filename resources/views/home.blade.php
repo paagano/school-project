a08438caafd1dbc29e814cf366c55c13bf9a94c1
Philip Agano <philip.agano@yahoo.com>
2022-07-04 16:56:52 +0300

    inititial modification of the home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kenya Trail Cast | A Domestic Travel Directory and Guide</title>
+</head>
+<body>
+    <h1>Welcome to my school project</h1>
+</body>
+</html>
